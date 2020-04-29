1cb0700480b72a34300f41f64a923c2f7d3b3880
Daniil Osmolovskiy <daniel.osmolovskiy@cience.com>
2020-04-29 12:05:30 +0300

    should work

--- chat/server.js
+++ chat/server.js
@@ -1,8 +1,16 @@
 const express = require('express');
+const favicon = require('express-favicon');
+const path = require('path');
+const port = process.env.PORT || 9999;
 const formatMessage = require('./src/messages')
 const { userJoin, getCurrentUser} = require('./src/users')
 
 const app = express();
+app.use(favicon(__dirname + '/build/favicon.png')); 
+
+app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, 'build')));
+
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
 
@@ -36,17 +44,12 @@ io.on('connection', socket => {
   })
 })
 
-server.listen(process.env.PORT || 9999, function(){
+
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  });
+  // app.listen(port);
+
+server.listen(port, function(){
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
-// const path = require('path');
-// const port = process.env.PORT || 9999;
-// const publicPath = path.join(__dirname, 'public');
-
-// app.use(express.static(publicPath));
-// app.get('*', (req, res) => {
-//    res.sendFile(path.join(publicPath, 'index.html'));
-// });
-// app.listen(port, () => {
-//    console.log('Server is up!');
-// });
